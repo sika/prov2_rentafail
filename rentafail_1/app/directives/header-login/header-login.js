fb519c32c1485330f98515b18c89b35e27f0ff7f
sika <simon.noworkemail@gmail.com>
2015-12-17 21:13:30 +0100

    added link to directive

--- rentafail_1/app/directives/header-login/header-login.js
+++ rentafail_1/app/directives/header-login/header-login.js
@@ -1,18 +1,21 @@
 ﻿var mHeaderLogin = angular.module('app.directives.mHeaderLogin', ['mMain'])// mMain-dependent due to factory call
 .directive('dHeaderLogin', fHeaderLogin);
 function fHeaderLogin() {
-	console.log("login");
+    console.log("login");
     return {
         restrict: 'A',
         scope: {}, //isolate scope
         templateUrl: 'app/directives/header-Login/header-Login.html',
-		controller: function($scope, $state, simpleFactory){
-			/* console.log(simpleFactory); */
-			$scope.users = simpleFactory.getUsers();
-			var bLogin = false;
-			$scope.bLogin = bLogin
-			console.log($scope.bLogin);
-			//console.log($scope.users);
-		}
-	}
+        controller: function ($scope, simpleFactory) {
+            /* console.log(simpleFactory); */
+            $scope.users = simpleFactory.getUsers();
+            var bLogin = false;
+            $scope.bLogin = bLogin
+            console.log($scope.bLogin);
+            //console.log($scope.users);
+        },
+        link: function (scope, element, attrs) {
+
+        }
+    }
 }
