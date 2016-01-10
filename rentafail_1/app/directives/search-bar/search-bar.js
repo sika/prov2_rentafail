ddc9851b155696c6f58ca9dfa8a25f21ff4d6240
sika <simon.noworkemail@gmail.com>
2016-01-10 23:38:25 +0100

    Added search scope

--- rentafail_1/app/directives/search-bar/search-bar.js
+++ rentafail_1/app/directives/search-bar/search-bar.js
@@ -1,12 +1,13 @@
-angular.module('app.directives.mSearchBar', ['mMain'])
+angular.module('app.directives.mSearchBar', [])
 .directive('dSearchBar', function(){
 	return{
 			restrict: 'A',
 			templateUrl: 'app/directives/search-bar/search-bar.html',
 			controller: 'mainController',
 			link: function (scope, element, attr, ctrl){
-				console.log(ctrl);
-				console.log(scope.users[0]);
+				//console.log(ctrl);
+				//console.log(scope.users[0]);
+				scope.search = "";
 			}
      
 		}
