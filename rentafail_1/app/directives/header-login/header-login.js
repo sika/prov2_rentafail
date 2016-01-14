11adb04d8af3f86efb375625b7b02ea696d78f71
sika <simon.noworkemail@gmail.com>
2016-01-14 22:11:49 +0100

    working login.html submit

--- rentafail_1/app/directives/header-login/header-login.js
+++ rentafail_1/app/directives/header-login/header-login.js
@@ -1,28 +1,19 @@
-﻿var mHeaderLogin = angular.module('app.directives.mHeaderLogin', ['mMain'])// mMain-dependent due to factory call
+﻿var mHeaderLogin = angular.module('app.directives.mHeaderLogin', [])// mMain-dependent due to factory call
 .directive('dHeaderLogin', fHeaderLogin);
 function fHeaderLogin() {
     return {
         restrict: 'A',
-        //replace: true,
-        //scope: {}, //isolate scope
         templateUrl: 'app/directives/header-Login/header-Login.html',
-/*         controller: function ($scope, mainFactory) {
-            $scope.users = mainFactory.getUsers();
-			//$scope.username = "test";
-			console.log($scope.$id);
-        }, */
-        link: function (scope, element, attrs, ctrl) {
-			
-            scope.login = { opLogin: false };
-			scope.username = "test";
-			console.log(scope);
-			scope.submit = function () {
-                console.log(scope.username);
-                console.log("fSubmit");
+        controller: function($scope){
+            $scope.loggedin = false;
+            $scope.username = { name: "aa" };
+            $scope.password = { pass: "bb" };
+        },
+        link: function (scope, element, attrs) {
+            scope.btn = function () {
+                console.log(scope.username.name);
+                alert(scope.username.name);
             }
-			scope.btn = function (){
-				alert(scope.username.name);
-			}
         }
     }
 }
